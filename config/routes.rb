6c806100737b6fc46e9cee705dd22262816f2da4
minhln1284 <luong.ngoc.minh@sun-asterisk.com>
2023-04-17 10:29:15 +0700

    Rails & React auth app

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "static#home"
+  resources :sessions, only: :create
+  delete :logout, to: "sessions#logout"
+  get :logged_in, to: "sessions#logged_in"
+  resources :registrations, only: :create
+end
